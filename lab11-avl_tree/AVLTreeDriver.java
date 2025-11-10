aae9ea094aefb9ae0b74eed9c9d164ef36c2ff0a
renesabm <renesabm@gmail.com>
2025-11-09 20:22:32 -0800

    Add files via upload

---
+++ lab11-avl_tree/AVLTreeDriver.java
@@ -0,0 +1,40 @@
+package com.mycompany.lab_project_11;
+
+
+import com.mycompany.lab_project_11.AVLTree;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author panka
+ */
+public class AVLTreeDriver {
+    public static void main(String[] args) {
+        AVLTree tree = new AVLTree();
+
+        // TODO: Insert test values
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        // TODO: Add more insertions to test all 4 rotation cases
+        tree.insert(5);
+        tree.insert(4); 
+        tree.insert(8);
+        tree.insert(25);
+        // TODO: Print traversals
+        System.out.print("Inorder: "); // Expected: 4 5 8 10 20 25 30 
+        tree.inorder();
+
+        System.out.print("Preorder: "); // Expected: 10 5 4 8 25 20 30
+        tree.preorder();
+
+        System.out.print("Postorder: "); // Expected: 4 8 5 20 30 25 10 
+        tree.postorder();
+
+        // TODO: Add expected results as comments for verification
+    }
+}
