f12ecc440fb537cb7d7ca2d8abede45013ba855e
renesabm <renesabm@gmail.com>
2025-12-06 19:02:09 -0800

    Add files via upload

--- Exam3-Capstone_Project/MaxHeap.java
+++ Exam3-Capstone_Project/MaxHeap.java
@@ -20,20 +20,64 @@ public class MaxHeap {
     
     public void insert(Task task) {
         // TODO- inserts a new task into the heap
+        heap.add(task);
+        upheap(heap.size()-1);
     }
     
     public Task extractMax() {
-        return null; //placeholder
-        // returns mask prioirty task
+        if (heap.isEmpty()) {
+            return null;
+        }
+
+        Task max = heap.get(0);
+
+        Task last = heap.remove(heap.size() - 1);
+
+        if (!heap.isEmpty()) {
+            heap.set(0, last);
+            downheap(0);
+        }
+
+        return max;
+        // returns max prioirty task
         
     }   
     
     public void upheap(int index) {
         // TODO- restores heap property afrer element inserted
+        
+        while (index>0) {
+            int parent = (index - 1) / 2;
+            int p = parent;
+            if (heap.get(p).compareTo(heap.get(index))>=0) {
+                break;
+            }
+            swap(index,p);
+            index=p;
+            
+        }
     }
     
     public void downheap(int index) {
         // TODO- restores heap property after element removed
+         
+        while (true) {
+            int left = 2 * index + 1; 
+            int right = 2 * index + 2;
+            int largest = index;
+ 
+            if (left < heap.size() && heap.get(left).compareTo(heap.get(largest)) > 0) {
+                largest=left;
+            }
+            if (right < heap.size() && heap.get(right).compareTo(heap.get(largest)) > 0) {
+            largest = right;
+            }
+            if (largest == index) {
+            break; 
+            }
+            swap(index, largest);
+            index = largest;
+        }
     }
     
     public void swap(int i, int j) {
